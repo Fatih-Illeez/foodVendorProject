044827f2aedc8cffc1e37f785d456a983ffcfdc9
Fatih Illeez <101408078+Fatih-Illeez@users.noreply.github.com>
2024-01-09 15:10:21 -0500

    vendor and customer dashboard edits

--- accounts/views.py
+++ accounts/views.py
@@ -9,6 +9,7 @@ from .utils import detectUser, send_verification_email
 from django.contrib.auth.decorators import login_required, user_passes_test
 from django.core.exceptions import PermissionDenied
 from django.contrib.auth.tokens import default_token_generator
+from vendor.models import Vendor
 
 
 
